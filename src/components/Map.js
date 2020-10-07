100bc1581cce304c89b57ef63d42609725bb9bcb
Jack-Tilley <tilley.e.jack@gmail.com>
2020-10-07 15:51:09 -0400

    added the ability to create markers

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,17 +1,28 @@
-import React, { useState } from 'react'
-import { GoogleMap, useLoadScript, DrawingManager } from '@react-google-maps/api'
+import React, { useState, useCallback } from 'react'
+import { GoogleMap, useLoadScript, DrawingManager, Marker } from '@react-google-maps/api'
+
 
 const libraries = ['drawing']
 
 const Map = () => {
   const [ myMap, setMyMap ] = useState(null);
   const [ center, setCenter ] = useState({ lat: 29.972065, lng: -90.111533 });
+  const [ markers, setMarkers ] = useState([]);
+
 
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
     libraries,
   });
 
+  const onMapClick = useCallback((e) => {
+    setMarkers(current => [...current, {
+        lat: e.latLng.lat(),
+        lng : e.latLng.lng(),
+        time : new Date()
+    }])
+  }, [])
+    
 
   const renderMap = () => (
         <GoogleMap
@@ -23,7 +34,14 @@ const Map = () => {
           zoom={10}
           center={center}
           onLoad={map => setMyMap(map)}
+          onClick={onMapClick}
         >
+            {markers.map((marker => (<Marker 
+              key={marker.time.toISOString()} 
+              position={{lat: marker.lat, lng: marker.lng}}     
+              />
+            )))}
+
             <DrawingManager />
         </GoogleMap>
   )
